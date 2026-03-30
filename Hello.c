063ac88de21ff11641626ad998094a536340efd1
Prajwal2930 <150530504+Prajwal2930@users.noreply.github.com>
2026-03-30 09:57:13 +0530

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 int main(){
-  printf("hello world - i came from github\n");
-  printf("automation test1\n");
-   printf("automation test2 autrev works!!!!\n");
+  printf("hello prajwal - i came from github\n");
   return 0;
 }
